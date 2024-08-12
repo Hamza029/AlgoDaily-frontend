f5622d0b33a5de6d812bc69d3bf15af945f4537c
Hamza029 <nahinhamza029@gmail.com>
2024-08-12 17:52:27 +0600

    added button tests

---
+++ src/components/Button/__tests__/Button.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from "@testing-library/react";
+import Button from "../Button";
+import { BUTTON_COLOR } from "../../../config/constants";
+
+describe("Button", () => {
+  it("Should render button component with correct label", () => {
+    render(
+      <Button handleClick={() => {}} color={BUTTON_COLOR.GRAY}>
+        Login
+      </Button>,
+    );
+    const btnComponent = screen.getByText("Login");
+    expect(btnComponent).toBeVisible();
+  });
+
+  it("Button should be disabled when disabled props is true", () => {
+    render(
+      <Button handleClick={() => {}} color={BUTTON_COLOR.GRAY} disabled={true}>
+        Login
+      </Button>,
+    );
+    const btn = screen.getByRole("button");
+    expect(btn).toBeDisabled();
+  });
+
+  it("Button should be disabled when disabled button component is loading", () => {
+    render(
+      <Button handleClick={() => {}} color={BUTTON_COLOR.GRAY} isLoading={true}>
+        Login
+      </Button>,
+    );
+    const btn = screen.getByRole("button");
+    expect(btn).toBeDisabled();
+  });
+});
--- src/components/Toast/__tests__/Toast.test.tsx
+++ src/components/Toast/__tests__/Toast.test.tsx
@@ -1,16 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import Toast from "../Toast";
-import { expect } from "vitest";
 
-it("Should render a Toast with correct message", () => {
-  render(
-    <Toast
-      message="Succesfully signed up"
-      severity="success"
-      handleToastClose={() => {}}
-    />,
-  );
-  const toastElement = screen.getByText("Succesfully signed up");
-  expect(toastElement).toBeInTheDocument();
-  expect(toastElement).toBeVisible();
+describe("Toast", () => {
+  it("Should render a Toast with correct message", () => {
+    render(
+      <Toast
+        message="Succesfully signed up"
+        severity="success"
+        handleToastClose={() => {}}
+      />,
+    );
+    const toastElement = screen.getByText("Succesfully signed up");
+    console.log(toastElement);
+    expect(toastElement).toBeInTheDocument();
+    expect(toastElement).toBeVisible();
+  });
 });
